41fa03abeeaecdfd582fd2460c1d62b1f6b75bb7
Luk1Pr0 <lpytel16@gmail.com>
2021-02-16 15:16:26 +0100

    Items can now be deleted and either completed or not

--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import TodoItem from './TodoItem';
 
-const List = ({ todos }) => {
+const List = ({ setTodos, todos }) => {
 	return (
 		<div className="todo-container">
 			<ul className="todo-list">
-				{todos.map(todo => <TodoItem key={todo.id} text={todo.text} />)}
+				{todos.map(todo => <TodoItem key={todo.id} text={todo.text} setTodos={setTodos} todos={todos} todo={todo} />)}
 			</ul>
 		</div>
 	)
