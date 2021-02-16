b633c20406f8a0d1e1c8f57560d6ffb84421c6d3
Luk1Pr0 <lpytel16@gmail.com>
2021-02-16 13:06:11 +0100

    Added state to the app for form submit and change events on the input

--- src/App.js
+++ src/App.js
@@ -6,15 +6,14 @@ import List from './components/List';
 
 const App = () => {
 	const [inputText, setInputText] = useState('');
-
-	console.log(inputText);
+	const [todos, setTodos] = useState([]);
 
 	return (
 		<div className="App">
 			<header>
 				<h1>To-do List</h1>
 			</header>
-			<Form setInputText={setInputText} />
+			<Form setInputText={setInputText} inputText={inputText} todos={todos} setTodos={setTodos} />
 			<List />
 		</div>
 	);
